55d3b2e10f27f724ee65d25a286652c15f63a63c
HuzefaBadshah <501huzefa@gmail.com>
2019-05-13 17:26:11 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
 const App = () => {
     return (
-        <div>App Works!</div>
+        <BrowserRouter>
+            <div>
+                <Route path="/" exact component={StreamList} />
+            </div>
+        </BrowserRouter>
     );
 }
 
