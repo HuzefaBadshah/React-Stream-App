f38356d5e17c552d7a5d7ac10d3801fb25671119
HuzefaBadshah <501huzefa@gmail.com>
2019-05-20 17:20:38 +0530

    updates....

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,32 +1,32 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchStreams } from "../../actions";
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 class StreamList extends Component {
   componentDidMount() {
     this.props.fetchStreams();
   }
 
   renderAdmin = (stream) => {
-    if(stream.userId === this.props.currentUserId) {
-        return (
-            <div className="right floated content">
-              <button className="ui button primary">Edit</button>
-              <button className="ui button negative">Delete</button>
-            </div>
-          );
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className="right floated content">
+          <Link to={`/streams/edit/${stream.id}`} className="ui button primary">Edit</Link>
+          <button className="ui button negative">Delete</button>
+        </div>
+      );
     }
   }
 
   renderCreate = () => {
-    if(this.props.isSignedIn) {
-        return (
-            <div className="{{textAlign: 'right'}}">
-                <Link to="/streams/new" className="ui button primary" >
-                    Create Stream
+    if (this.props.isSignedIn) {
+      return (
+        <div style={{ textAlign: 'right' }}>
+          <Link to="/streams/new" className="ui button primary" >
+            Create Stream
                 </Link>
-            </div>
-        );
+        </div>
+      );
     }
   }
 
@@ -34,7 +34,7 @@ class StreamList extends Component {
     return this.props.streams.map(item => {
       return (
         <div className="item" key={item.id}>
-        {this.renderAdmin(item)}
+          {this.renderAdmin(item)}
           <i className="large middle aligned icon camera" />
           <div className="content">
             {item.title}
